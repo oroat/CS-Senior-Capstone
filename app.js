d619d9ffe446c48635775eeef45dda7258734eb1
dpmorales7 <112967576+dpmorales7@users.noreply.github.com>
2026-02-03 18:50:22 -0500

    Create app.js
    app created and has functionailty

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+const app = express();
+
+/** 
+const sessionM = session({
+    secret: "yourSecretKey",
+    resave: false,
+    saveUninitialized: false,
+});
+*/
+
+
+//app.use(sessionM);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public_html")));
+
+const testPath = path.join(__dirname, "public_html", "landing.html");
+
+app.get("/", (req, res) => res.sendFile(testPath));
+
+
+module.exports = app;
