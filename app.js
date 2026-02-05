db8020ae1ba9324180218ae6f32a69f0e9bba16c
oscarroat <oroat@loyola.edu>
2026-02-05 15:55:28 -0500

    added delete button to each user in list

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.post('/registeruser', UserController.register);
 
 app.get('/users', UserController.getAllUsers);
 
-app.get('/deleteuser/:id', UserController.deleteUser);
+app.delete('/deleteuser/:id', UserController.deleteUser);
 
 
 module.exports = app;
