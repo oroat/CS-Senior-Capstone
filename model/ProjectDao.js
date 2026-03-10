64ddd537d8d6a362abd4d420e478448af5fc3f98
dylanmorales <dpmorales@loyola.edu>
2026-03-10 01:29:43 -0400

    un commented material related stuff #2 #5

--- model/ProjectDao.js
+++ model/ProjectDao.js
@@ -1,73 +1,32 @@
 const mongoose = require('mongoose');
 
 const projectSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    manager: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    workers: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'users'
-    }],
-    //once matrials is contructed remove comments
-    /** 
+    name: { type: String, required: true },
+    manager: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
+    location: { type: String, required: true },
+    workers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
     materials: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "materials"
     }]  
-    */
 });
 
 const projectModel = mongoose.model('projects', projectSchema);
 
-
 exports.readAll = async function(){
-    let projects = await projectModel.find()
+    return await projectModel.find()
         .populate('manager')
-        .populate('workers');
-        // .populate('materials');  //remove the comments once materials is constructed
-    return projects;
+        .populate('workers')
+        .populate('materials');
 }
 
 exports.read = async function(id){
-    let project = await projectModel.findById(id)
+    return await projectModel.findById(id)
         .populate('manager')
-        .populate('workers');
-        // .populate('materials');  //remove the comments once materials is constructed
-    return project;
+        .populate('workers')
+        .populate('materials');
 }
 
-exports.create = async function(newProject){
-    let project = new projectModel(newProject);
-    await project.save();
-    return project;
-}
-
-exports.update = async function(id, updates){
-    let project = await projectModel.findByIdAndUpdate(
-        id,
-        updates,
-        { new: true }
-    );
-    return project;
-}
-
-exports.del = async function(id){
-    let project = await projectModel.findByIdAndDelete(id);
-    return project;
-}
-
-exports.deleteAll = async function(){
-    await projectModel.deleteMany();
-}
-
-
+exports.create = async (data) => await new projectModel(data).save();
+exports.update = async (id, updates) => await projectModel.findByIdAndUpdate(id, updates, { new: true });
+exports.del = async (id) => await projectModel.findByIdAndDelete(id);
