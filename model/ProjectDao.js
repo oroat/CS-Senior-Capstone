5125b2104997df21f84add03bb9aefc60584899c
dpmorales7 <112967576+dpmorales7@users.noreply.github.com>
2026-03-31 03:23:44 -0400

    Remove materials field from ProjectDao schema #4
    Removed materials field from project schema and related population.

--- model/ProjectDao.js
+++ model/ProjectDao.js
@@ -5,10 +5,6 @@ const projectSchema = new mongoose.Schema({
     manager:  { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
     location: { type: String, required: true },
     workers:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
-    materials: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "materials"
-    }]
 });
 
 const projectModel = mongoose.model('projects', projectSchema);
@@ -17,14 +13,14 @@ exports.readAll = async function(){
     return await projectModel.find()
         .populate('manager')
         .populate('workers')
-        .populate('materials');
+        
 }
 
 exports.read = async function(id){
     return await projectModel.findById(id)
         .populate('manager')
         .populate('workers')
-        .populate('materials');
+        
 }
 
 exports.create = async (data) => await new projectModel(data).save();
