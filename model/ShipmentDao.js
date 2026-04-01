c44834cafb42a8df71ef5f4b21b8751ad6b50811
dylanmorales <dpmorales@loyola.edu>
2026-03-31 22:58:02 -0400

    Changes made to better handle new purchase order changes and check accuracy #11 #12

--- model/ShipmentDao.js
+++ model/ShipmentDao.js
@@ -1,55 +1,50 @@
 const mongoose = require('mongoose');
 
 const shipmentSchema = new mongoose.Schema({
-    sender: { type: String, required: true },
-    project: { type: mongoose.Schema.Types.ObjectId, ref: "projects", required: true },
+    sender:   { type: String, required: true },
+    project:  { type: mongoose.Schema.Types.ObjectId, ref: 'projects', required: true },
+    po:       { type: mongoose.Schema.Types.ObjectId, ref: 'purchaseorders', required: true },
     dt_recvd: { type: String, required: true },
-    recip: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
+    recip:    { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
+    // Materials are stored as plain objects (snapshot from the PO at time of shipment)
     materials_recvd: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "materials"
+        materialId: { type: String },
+        name:       { type: String },
+        unit:       { type: String },
+        quantity:   { type: Number, default: 0 }
     }]
 });
 
 const shipmentModel = mongoose.model('shipments', shipmentSchema);
 
-exports.readAll = async function() {
-    let shipments = await shipmentModel.find()
+exports.readAll = async function () {
+    return await shipmentModel.find()
         .populate('project')
         .populate('recip')
-        .populate('materials_recvd');
-    return shipments;
-}
+        .populate('po');
+};
 
-exports.read = async function(id) {
-    let shipment = await shipmentModel.findById(id)
+exports.read = async function (id) {
+    return await shipmentModel.findById(id)
         .populate('project')
         .populate('recip')
-        .populate('materials_recvd');
-    return shipment;
-}
+        .populate('po');
+};
 
-exports.create = async function(newShipment) {
+exports.create = async function (newShipment) {
     let shipment = new shipmentModel(newShipment);
     await shipment.save();
     return shipment;
-}
+};
 
-exports.update = async function(id, updates) {
-    let shipment = await shipmentModel.findByIdAndUpdate(
-        id,
-        updates,
-        { new: true }
-    );
-    return shipment;
-}
+exports.update = async function (id, updates) {
+    return await shipmentModel.findByIdAndUpdate(id, updates, { new: true });
+};
 
-exports.del = async function(id) {
-    let shipment = await shipmentModel.findByIdAndDelete(id);
-    return shipment;
-}
+exports.del = async function (id) {
+    return await shipmentModel.findByIdAndDelete(id);
+};
 
-exports.deleteAll = async function() {
+exports.deleteAll = async function () {
     await shipmentModel.deleteMany();
-}
-
+};
